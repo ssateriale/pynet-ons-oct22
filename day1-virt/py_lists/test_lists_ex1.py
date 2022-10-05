da71dcb0fe940d2c41e80114178163a9a9122723
Steve Sateriale <stevesateriale@gmail.com>
2022-10-05 16:54:23 -0400

    Addinf test list file 1

---
+++ day1-virt/py_lists/test_lists_ex1.py
@@ -0,0 +1,10 @@
+#!
+
+my_list = ["server1", "wap", "switch3", "switch1", "server2"]
+my_list.append("router")
+my_list.append("server4")
+print(my_list.pop(0))
+
+print(f"Length of list: {len(my_list)}")
+my_list.sort()
+print(my_list)
