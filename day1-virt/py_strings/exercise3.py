fd62e434029b5d1a462b00bb192c0e912c37db77
Steve Sateriale <stevesateriale@gmail.com>
2022-10-18 16:40:18 -0400

    lesson 1 exercise 3

---
+++ day1-virt/py_strings/exercise3.py
@@ -0,0 +1,8 @@
+ipv6_addr1 = "2001:db8:1234::1"
+IPV6_ADDR2 = "2001:db8:1234::2"
+ipV6_addR3 = "2001:db8:1234::3"
+
+print("")
+print("Is variable1 equal to variable2: {}".format(ipv6_addr1 == IPV6_ADDR2))
+print("Is variable1 not equal to variable3: {}".format(ipv6_addr1 != ipV6_addR3))
+print("")
