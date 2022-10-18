3dad3ab70cc38c0ff5bfa70357210377103e3f7a
Steve Sateriale <stevesateriale@gmail.com>
2022-10-18 16:38:12 -0400

    Lesson 1 commits

---
+++ day1-virt/py_strings/exercise1.py
@@ -0,0 +1,5 @@
+ipaddr1 = "192.168.16.1"
+ipaddr2 = "10.10.1.1"
+ipaddr3 = "172.16.31.17"
+
+print(ipaddr1, ipaddr2, ipaddr3)
---
+++ day1-virt/py_strings/exercise2.py
@@ -0,0 +1,30 @@
+try:
+    # PY2
+    ip_addr = raw_input("Please enter an IP address: ")
+except NameError:
+    # PY3
+    ip_addr = input("Please enter an IP address: ")
+
+octets = ip_addr.split(".")
+print()
+print("{:^15}{:^15}{:^15}{:^15}".format("Octet1", "Octet2", "Octet3", "Octet4"))
+print("-" * 60)
+print("{:^15}{:^15}{:^15}{:^15}".format(*octets))
+print(
+    "{:^15}{:^15}{:^15}{:^15}".format(
+        bin(int(octets[0])),
+        bin(int(octets[1])),
+        bin(int(octets[2])),
+        bin(int(octets[3])),
+    )
+)
+print(
+    "{:^15}{:^15}{:^15}{:^15}".format(
+        hex(int(octets[0])),
+        hex(int(octets[1])),
+        hex(int(octets[2])),
+        hex(int(octets[3])),
+    )
+)
+print("-" * 60)
+print()
