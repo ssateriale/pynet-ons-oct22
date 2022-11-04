68d8ac73024787e4ebb4cc529033e68a98e435e5
Steve Sateriale <stevesateriale@gmail.com>
2022-11-04 11:23:21 -0400

    Functions exercise 3

---
+++ day3/functions/function_ex3.py
@@ -0,0 +1,14 @@
+def my_func(var1, var2, var3, var4):
+    print()
+    print(f"{var1=}")
+    print(f"{var2=}")
+    print(f"{var3=}")
+    print(f"{var4=}")
+    print()
+
+
+my_func(1, 2, 3, 4)
+
+my_func(var4=4, var1=1, var2=2, var3=3)
+
+my_func(1, var3=3, var4=4, var2=2)
